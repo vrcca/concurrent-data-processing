a0072e982410b295a7701a3bbd067169db3a2a45
Vitor Cavalcanti <1920397+vrcca@users.noreply.github.com>
2021-02-07 16:03:03 +0100

    Setups task supervisor

--- lib/sender.ex
+++ lib/sender.ex
@@ -1,13 +1,23 @@
 defmodule Sender do
+  def send_email(email = "konnichiwa@world.com") do
+    raise "Oops, couldn't send email to #{email}!"
+  end
+
   def send_email(email) do
-    Process.sleep(3000)
-    IO.puts("Email to #{email} sent")
+    time = Enum.random(2_000..4_000)
+    Process.sleep(time)
+    IO.puts("Email to #{email} sent in #{time} milliseconds.")
     {:ok, "email_sent"}
   end
 
   def notify_all(emails) do
-    Enum.each(emails, fn email ->
-      Task.start(fn -> send_email(email) end)
-    end)
+    Sender.EmailTaskSupervisor
+    |> Task.Supervisor.async_stream_nolink(emails, &send_email/1,
+      max_concurrency: 2,
+      ordered: false,
+      timeout: 3_000,
+      on_timeout: :kill_task
+    )
+    |> Enum.to_list()
   end
 end
