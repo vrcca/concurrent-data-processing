202abf66863633f0181ee60e07b4505a4e9a032f
Vitor Cavalcanti <1920397+vrcca@users.noreply.github.com>
2021-03-05 16:10:14 +0100

    Improves airports app by using Streams

--- lib/airports/airports.ex
+++ lib/airports/airports.ex
@@ -7,16 +7,19 @@ defmodule Airports.Airports do
 
   def open_airports() do
     airports_csv()
-    |> File.read!()
-    |> CSV.parse_string()
-    |> Enum.map(fn row ->
+    |> File.stream!()
+    |> CSV.parse_stream()
+    |> Stream.map(fn row ->
+      # :binary.copy is required if you want to keep the data from CSV
+      # because row is just a reference to the file (which is good for performance)
       %{
-        id: Enum.at(row, 0),
-        type: Enum.at(row, 2),
-        name: Enum.at(row, 3),
-        country: Enum.at(row, 8)
+        id: :binary.copy(Enum.at(row, 0)),
+        type: :binary.copy(Enum.at(row, 2)),
+        name: :binary.copy(Enum.at(row, 3)),
+        country: :binary.copy(Enum.at(row, 8))
       }
     end)
-    |> Enum.reject(&(&1.type == "closed"))
+    |> Stream.reject(&(&1.type == "closed"))
+    |> Enum.to_list()
   end
 end
