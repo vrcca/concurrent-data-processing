56b7ee2e615fcf74dd9a08a9b0451fa402c77daa
Vitor Cavalcanti <1920397+vrcca@users.noreply.github.com>
2021-05-14 11:41:17 +0200

    Replaces GenStage with Flow

--- lib/scraper/online_page_producer_consumer.ex
+++ lib/scraper/online_page_producer_consumer.ex
@@ -1,28 +1,17 @@
 defmodule Scraper.OnlinePageProducerConsumer do
-  require Logger
-  use GenStage
+  use Flow
   alias Scraper.PageProducer
+  alias Scraper.PageConsumerSupervisor
 
-  def start_link(id) do
-    initial_state = []
-    GenStage.start_link(__MODULE__, initial_state, name: via(id))
-  end
+  def start_link(_args) do
+    producers = [Process.whereis(PageProducer)]
 
-  def via(id), do: {:via, Registry, {ProducerConsumerRegistry, id}}
-
-  def init(initial_state) do
-    Logger.info("OnlinePageProducerConsumer init")
-
-    producers = [
-      {PageProducer, min_demand: 0, max_demand: 1}
+    consumers = [
+      {Process.whereis(PageConsumerSupervisor), max_demand: 2}
     ]
 
-    {:producer_consumer, initial_state, subscribe_to: producers}
-  end
-
-  def handle_events(events, _from, state) do
-    Logger.info("OnlinePageProducerConsumer received #{inspect(events)}")
-    online_events = Enum.filter(events, &Scraper.online?/1)
-    {:noreply, online_events, state}
+    Flow.from_stages(producers, max_demand: 1, stages: 2)
+    |> Flow.filter(&Scraper.online?/1)
+    |> Flow.into_stages(consumers)
   end
 end
--- lib/scraper/page_consumer_supervisor.ex
+++ lib/scraper/page_consumer_supervisor.ex
@@ -2,10 +2,9 @@ defmodule Scraper.PageConsumerSupervisor do
   use ConsumerSupervisor
   require Logger
   alias Scraper.PageConsumer
-  alias Scraper.OnlinePageProducerConsumer
 
   def start_link(args) do
-    ConsumerSupervisor.start_link(__MODULE__, args)
+    ConsumerSupervisor.start_link(__MODULE__, args, name: __MODULE__)
   end
 
   def init(_args) do
@@ -15,12 +14,7 @@ defmodule Scraper.PageConsumerSupervisor do
       %{id: PageConsumer, start: {PageConsumer, :start_link, []}, restart: :transient}
     ]
 
-    producers = [
-      {OnlinePageProducerConsumer.via("online_page_producer_consumer_1"), []},
-      {OnlinePageProducerConsumer.via("online_page_producer_consumer_2"), []}
-    ]
-
-    opts = [strategy: :one_for_one, subscribe_to: producers]
+    opts = [strategy: :one_for_one, subscribe_to: []]
 
     ConsumerSupervisor.init(children, opts)
   end
