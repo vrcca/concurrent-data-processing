5cdc2f5204d42fd2e384583a176ab1e666b37186
Vitor Cavalcanti <1920397+vrcca@users.noreply.github.com>
2021-05-14 10:44:59 +0200

    Partitions flows using :key and uses take_sort

--- lib/airports/airports.ex
+++ lib/airports/airports.ex
@@ -21,6 +21,36 @@ defmodule Airports.Airports do
       }
     end)
     |> Flow.reject(&(&1.type == "closed"))
+    |> Flow.partition(key: {:key, :country})
+    |> Flow.group_by(& &1.country)
+    |> Flow.map(fn {country, data} -> {country, Enum.count(data)} end)
+    |> Flow.take_sort(10, fn {_, a}, {_, b} -> a > b end)
     |> Enum.to_list()
+    |> List.flatten()
   end
 end
+
+# RESULTS
+#
+# # Naive Enum
+# iex(2)> :timer.tc(&Airports.Airports.open_airports/0)
+# {3_540_285, ...}
+#
+# iex(1)> :timer.tc(&Airports.Airports.open_airports/0)
+# {933_022, ...}
+#
+# # Stream
+# iex(3)> :timer.tc(&Airports.Airports.open_airports/0)
+# {2_503,
+#
+# # Flow - naive
+# iex(5)> :timer.tc(&Airports.Airports.open_airports/0)
+# {916_531,
+#
+# # Flow - parse CSV in map
+# iex(7)> :timer.tc(&Airports.Airports.open_airports/0)
+# {891_809,
+#
+# # Flow + read_ahead
+# iex(9)> :timer.tc(&Airports.Airports.open_airports/0)
+# {1_020_540,
